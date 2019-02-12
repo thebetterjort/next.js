d129d03bd38defe55b1062cb7d103373b5781392
thebetterjort <thebetterjort@gmail.com>
2019-02-11 23:29:48 -0600

    Day 1 - Fixing up somethings

---
+++ examples/hello-world/pages/day1/index.js
@@ -0,0 +1,65 @@
+import Link from "next/link";
+export default () => (
+  <div>
+    <h1>Day 1</h1>
+    <ol>
+      <h4>Table of Contents</h4>
+      <li>Getting into this mess</li>
+      <li>What I've learned so far</li>
+      <li>Research</li>
+    </ol>
+    <h3>1. Getting into this mess</h3>
+    <p>
+      What brings somebody to decide to grow the ocean inside of their house?
+      What type of person decides to put so much time and money into an often
+      times rearly viewed conversational piece. I decided to travel from
+      Northwest Arkansas to Columbia, Missouri to find out this answer.
+    </p>
+    <h4>Traveling 600 miles for answers</h4>
+    <p>
+      An old college friend, a Mizzou graduate has been setting up and maintaing
+      tanks for years. His current job is to handle waterflow and basically
+      engineers water systems for a living. My friend is an expert on water
+      systems, so when he said "I have a tank and stand for you" I was elated,
+      but petrified.
+    </p>
+    <p>
+      My friend is a mathmatically driven soul. Scientific until the day he dies
+      completely contrasts my shoot from the hip approach to life. He is
+      somebody that you meet and you say, "smart as hell". At the sametime, you
+      walk inside of his house and he has all sorts of plantlife growing inside
+      and outside of his house. I would call him a modern hippy.
+    </p>
+    <p>
+      There is no doubt that I will be in good hands as I walk (excuse me,
+      crawl) through this project. I thought as soon as I visited my friend I
+      would have a better sense of what I needed and have an Amazon shopping
+      cart ready to go. I was wrong.
+    </p>
+    <h4>Tank Selection</h4>
+    <p>
+      My friend gave me a 72 gallon bow front. It came with a stand that we had
+      to reinforce because it had seen a better time.
+    </p>
+    <p>
+      The tank fit into my Tesla Model 3, but unforntately was not able to get
+      the stand. That is coming up in the coming weeks.
+    </p>
+    <h3>What I've learned on day 1</h3>
+    <ol>
+      <li>The bigger the tank the easier it is to manage.</li>
+      <li>A sump is a tank that goes underneath your visible tank.</li>
+      <li>
+        The most important component when first starting out is something called
+        a protein skimmer
+      </li>
+      <li>Water dissolves but salt does not.</li>
+    </ol>
+
+    <h3>Next steps:</h3>
+    <p>Research protein skimmers and things that go in sumps</p>
+    <Link href="/">
+      <a>Home </a>
+    </Link>
+  </div>
+);
--- examples/hello-world/pages/index.js
+++ examples/hello-world/pages/index.js
@@ -1,4 +1,21 @@
-import Link from 'next/link'
+import Link from "next/link";
 export default () => (
-  <div>Hello World. <Link href='/about'><a>About</a></Link></div>
-)
+  <div>
+    <h1>Salinity & Hydration</h1>
+    <p>
+      This is a living document that will showcase the development cycle of a
+      salt water aquarium.
+    </p>
+    It's been said that building a salt water aquarium are rather difficult for
+    beginners.
+    <p />
+    <p>
+      The amount of information is daunting. A waterfall of information. This
+      site will showcase the difficulties that occur when somebody with zero
+      knowledge decides to endure on building a salt water aquarium.
+    </p>
+    <Link href="/day1">
+      <a>Day 1 - Research </a>
+    </Link>
+  </div>
+);
